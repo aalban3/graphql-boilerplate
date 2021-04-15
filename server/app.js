059b1ff08e3fc1fbc3f926affb4cfec373bbe531
Alan Alban <alan.alban10@gmail.com>
2021-04-15 15:15:10 -0400

    Added graphQL packages

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const app = express();
 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+  })
+);
 
 module.exports = app;
